f6de954c3ebd79f22e4ba161c73ac78c4bbfa27f
waldir j l marques <waldirwolf@outlook.com>
2022-01-15 21:56:55 -0300

    ok

--- primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -1,1 +1,7 @@
+import { Component } from "@angular/core";
 
+@Component({
+    selector: 'meu-primeiro-componet',
+    template: "<p>Meu primeiro component com Angula 2!</p>"
+})
+export class MeuPrimeiroComponent {}
